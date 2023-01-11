6fd4311ed777a24149b478b9b71a87306511952f
remy <remyciterin@gmail.com>
2023-01-11 23:52:59 +0100

    finish StateT : for T monad transformer, T W is the specification of T M

--- DijkstraMonad/State.lean
+++ DijkstraMonad/State.lean
@@ -7,66 +7,58 @@ def StateT (σ : Type u) (m : Type u → Type v) (α : Type u) : Type (max u v) 
 
 universe u v w
 
--- define a specification using weakest precondition of StateT from
--- a specification of m
-def StateT.wp (σ : Type u) (W:Type u → Type v) (α : Type u) : Type (max u v) :=
-  {f:(σ × α → Prop) → σ → Prop // ∀ p p' s, (∀ x, p x → p' x) → f p s → f p' s} × W α
 
-#print StateT.wp
-
-instance {W:Type u → Type v} {σ:Type u}
-  [Monad W]
-  [OrderedMonad W]
-  : Monad (StateT.wp σ W) where
-
-  pure {α:Type u} (x:α) : StateT.wp σ W α := (⟨
-    fun p s0 => p (s0, x),
-    by
-      intro p p' s h
-      simp
-      apply h
-    ⟩, pure x)
-
-  bind {α β:Type u} (m:StateT.wp σ W α) (f:α → StateT.wp σ W β) : StateT.wp σ W β :=
-    (⟨
-      fun p s0 => m.1.val (fun (s1, x) => (f x).1.val p s1) s0,
-      by
-        simp
-        intro p p' s h1 h2
-        apply m.1.property (fun x => (f x.2).1.val p x.1) (fun x => (f x.2).1.val p' x.1)
-        . intro x
-          apply (f x.2).1.2
-          assumption
-        . assumption
-    ⟩, bind m.2 $ fun x => (f x).2)
-
-#check OrderedMonad.refl
-#check OrderedMonad.trans
-
-
-instance {W:Type u → Type v} {σ:Type u}
+open Monad in
+open OrderedMonad in
+open EffectObservation in
+instance {σ:Type u}
+  {W:Type u → Type v}
   [monadW:Monad W]
-  [oMonadW:OrderedMonad W]
-  : OrderedMonad (StateT.wp σ W) where
+  [ordMonadW:OrderedMonad W]
+  : OrderedMonad (StateT σ W) where
 
-  leW {α:Type u} (p1 p2:StateT.wp σ W α) : Prop := (∀ p s, p2.1.1 p s → p1.1.1 p s) ∧ p1.2 ≤ᵂ p2.2
+  leW {α:Type u} (x y:StateT σ W α) := ∀ s, x s ≤ᵂ y s
 
-  refl {α:Type u} (p:StateT.wp σ W α) := by
-    constructor
-    . intros p' s h
-      assumption
-    . apply oMonadW.refl
+  trans := by
+    intro α a b c h1 h2 s
+    apply trans (a s) (b s) (c s)
+    apply h1 s
+    apply h2 s
 
-  trans {α:Type u} a b c := by
-    intro h1 h2
-    constructor
-    . intros
-      apply h1.1
-      apply h2.1
-      assumption
-    . apply oMonadW.trans _ _ _ h1.2 h2.2
+  refl := by
+    intro α a s
+    apply refl
 
   bindW := by
-    intro α β w w' f f' h1 h2
-    constructor
-    sorry
+    intro α β w w' f f' h1 h2 s
+    simp [bind, StateT.bind]
+    apply bindW
+    apply h1
+    intro x
+    apply h2
+
+open Monad in
+open OrderedMonad in
+open EffectObservation in
+instance {σ:Type u}
+  {M W:Type u → Type v}
+  [Monad M] [Monad W]
+  [OrderedMonad W]
+  [EffectObservation M W]
+  : EffectObservation (StateT σ M) (StateT σ W) where
+
+  θ {α:Type u} (m:StateT σ M α) : StateT σ W α := fun s => θ (m s)
+
+  bindθ := by
+    intro α β m f
+    simp [bind, pure, StateT.bind]
+    apply funext
+    intro x
+    apply bindθ
+
+  pureθ := by
+    intros α x
+    simp [pure, StateT.pure]
+    apply funext
+    intro s
+    apply pureθ
